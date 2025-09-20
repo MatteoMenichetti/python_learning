5db74356853aae1796011b7562224e00f445bd73
MatteoMenichetti <no.onemail.com>
2025-09-20 19:33:28 +0200

    aggiunte cose

---
+++ part-2.py
@@ -0,0 +1,13 @@
+n = [13, 11, 12]
+
+n.sort()
+
+S = 'Ciao mondo!'
+
+print(S)
+
+print(n)
+
+M = [[1, 2, 3], # A 3 × 3 matrix, as nested lists
+[4, 5, 6], # Code can span lines if bracketed
+[7, 8, 9]]

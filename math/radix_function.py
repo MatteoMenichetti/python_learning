d780acbefb12c20697d05ce2b40b6e6db2dd8062
MatteoMenichetti <no.onemail.com>
2025-09-30 18:04:05 +0200

    introdotti metodi di ricerca delle radici, tbf

---
+++ math/radix_function.py
@@ -0,0 +1,74 @@
+import math
+from symbol import return_stmt
+
+
+def bisection(f, a, b, tol):
+    """ bisection(f, a, b, tol)
+
+    Parameters
+    __________
+    - f, function (lambda or function, eg (lambda x: x**2))
+    - a, b, interval
+    - tol, tolerance
+
+    Returns
+    _________
+    - x, zero of f
+    """
+
+    if a - b == 0 | a > b: print('intervallo non valido', a, b); return None
+
+    if tol<=0: print('tolleranza', tol, 'non valida'); return None;
+
+    n = math.ceil(math.log2(abs(b-a)) - math.log2(tol))
+
+    fa = f(a); fb = f(b)
+
+    x = i = 0
+
+    for i in range(n):
+        x = (a + b) / 2
+
+        fx = f(x)
+
+        f1x = abs((fa - fb) / (b - a))
+
+        if fx == 0:
+            return [x,n]
+
+        if f1x == 0: print('f1x == 0'); return None
+
+        if abs(fx) <= f1x * tol: return [x,i]
+
+        if fa * fx < 0: b = x;
+
+        elif fb * fx < 0: a = x
+
+    return [x,i]
+
+def newton(f, f1, x, tol = 1e-15, itmax = 1000):
+    """ newton(f, a, b, tol, itmax)
+
+    Parameters
+    __________
+    - f, function (lambda or function, eg (lambda x: x**2))
+    - f1, derivate of f
+    - x, starting point
+    - tol, tolerance (default 1e-15)
+    - itmax, max number of iterations (default=1000)
+    """
+
+    if tol < 0: print('tolleranza', tol, 'non valida'); return None;
+
+    if itmax < 0: print('itmax', itmax, 'non valida'); return None;
+
+    for i in range(itmax):
+
+        f1x = f1(x)
+
+        if f1x == 0: print('f1x == 0'); return None
+
+        x1 = x - f(x)/f1x
+
+        if abs(x1-x) < tol: return [x,i]
+
